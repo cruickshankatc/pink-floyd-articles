adfc7b4b6a9df5a6b3472c24f4817102d8ad85d6
Andrew Cruickshank <cruickshankatc@gmail.com>
2024-02-19 02:19:37 -0500

    added bootstrap

--- frontend/pink-floyd-articles-app/src/App.js
+++ frontend/pink-floyd-articles-app/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/NavBar";
-import Home from "./pages/AboutPage";
+import Home from "./pages/HomePage";
 
 function App() {
   return (
     <div className="App">
+      <p>felidae</p>
       <Router>
         <div className='App'>
           <Navbar />
